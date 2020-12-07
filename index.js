f0f3273b1ca600124cf6d12f3c9fcf5226bdc988
Michael Chen <michaelchengaming@gmail.com>
2020-12-07 15:59:36 -0800

    refactor a bit and comment out unused code.

--- index.js
+++ index.js
@@ -8,88 +8,84 @@ client.commands = new Discord.Collection();
 client.mongoose = require('./utils/mongoose');
 client.logger = require('./utils/Logger');
 
-async function init() {
-  // Load events
-  // Load commands
-}
+// async function init() {
+// }
+// Load events
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error;
+  files.forEach((file) => {
+    if (!file.endsWith('.js')) return;
+    const evt = require(`./events/${file}`);
+    let evtName = file.split('.')[0];
+    console.log(`Loaded event '${evtName}'`);
+    client.on(evtName, evt.bind(null, client));
+  });
+});
+// Load commands
+fs.readdir('./commands/', async (err, files) => {
+  if (err) return console.error;
+  files.forEach((file) => {
+    if (!file.endsWith('.js')) return;
+    let props = require(`./commands/${file}`);
+    let cmdName = file.split('.')[0];
+    console.log(`Loaded command '${cmdName}'`);
+    client.commands.set(cmdName, props);
+  });
+});
 
 // Dig through commands directory for all the command files ending in .js
-const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
+// const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
+// for (const file of commandFiles) {
+//   const command = require(`./commands/${file}`);
+//   client.commands.set(command.name, command);
+// }
 
 // Using '!' for development, need a diff prefix for production
 const PREFIX = '!';
 
 // EVENTS
 // todo: move these events into its own directory
-client.on('ready', () => {
-  console.log(`Bot: ${client.user.tag}!`);
-});
+// client.on('ready', () => {
+//   console.log(`Bot: ${client.user.tag}!`);
+// });
 
 // Welcomes new member to server by DM
-client.on('guildMemberAdd', (member) => {
-  member.send(
-    'Welcome to the server. (Placeholder). Rule 1: Must behave Rule 2: Rule 3: ipsum lorem.... Answer these questions. Will you behave?.. (bot will ask a few verification questions?'
-  );
-});
+// client.on('guildMemberAdd', (member) => {
+//   member.send(
+//     'Welcome to the server. (Placeholder). Rule 1: Must behave Rule 2: Rule 3: ipsum lorem.... Answer these questions. Will you behave?.. (bot will ask a few verification questions?'
+//   );
+// });
 
 // Most of the magic happens here
-client.on('message', (msg) => {
-  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
-  processCommand(msg);
-});
+// client.on('message', (msg) => {
+//   if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+//   processCommand(msg);
+// });
 
 // Process the requested command
-function processCommand(message) {
-  const [commandName, ...args] = message.content
-    .trim()
-    .substring(PREFIX.length)
-    .split(/\s+/);
+// function processCommand(message) {
+//   const [commandName, ...args] = message.content
+//     .trim()
+//     .substring(PREFIX.length)
+//     .split(/\s+/);
 
-  if (!client.commands.has(commandName))
-    return message.reply(
-      'there was an error trying to execute that command! Try `!commands`'
-    );
+//   if (!client.commands.has(commandName))
+//     return message.reply(
+//       'there was an error trying to execute that command! Try `!commands`'
+//     );
 
-  const command = client.commands.get(commandName);
+//   const command = client.commands.get(commandName);
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
-  }
-}
+//   try {
+//     command.execute(message, args);
+//   } catch (error) {
+//     console.error(error);
+//     message.reply('there was an error trying to execute that command!');
+//   }
+// }
 
-// Connect to db
-// mongoose
-//   .connect(process.env.MONGODB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     autoIndex: false,
-//     reconnectTries: Number.MAX_VALUE,
-//     reconnectInterval: 500,
-//     poolSize: 5,
-//     connectTimeoutMS: 10000,
-//     family: 4,
-//   })
-//   .then(() => {
-//     client.logger.log('Connected to mongooo db.', 'log');
-//   })
-//   .catch((err) => {
-//     client.logger.log(`Error connecting to db. Error: ${err}`, 'error');
-//   });
 client.mongoose.init();
 client.login(process.env.BOT_TOKEN);
-
-// client
-//   .on('disconnect', () => client.logger.log('Bot is disconnecting...', 'warn'))
-//   .on('reconnecting', () => client.logger.log('Bot reconnecting...', 'log'))
-//   .on('error', (e) => client.logger.log(e, 'error'))
-//   .on('warn', (info) => client.logger.log(info, 'warn'));
 
 //For any unhandled errors
 process.on('unhandledRejection', (err) => {
