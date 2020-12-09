70f91cf9015ad237ca11471b5ef46748bf51b3b8
Michael Chen <michaelchengaming@gmail.com>
2020-12-09 14:18:55 -0800

    add match command, bot sends dm to msg author

--- commands/kick.js
+++ commands/kick.js
@@ -8,8 +8,6 @@ module.exports = {
 
   async execute(message, args) {
     try {
-      // console.log(message.);
-      console.log(args);
       if (!message.member.hasPermission('KICK_MEMBERS'))
         return message.reply('You do not have permissions to use that command.');
       if (args.length === 0) return message.reply('Please provide ID');
@@ -29,25 +27,6 @@ module.exports = {
         message.channel.send('That member was not found.');
       }
 
-      // TODOS: Move the big ole chunk of text into their own commands under ./commands/
-      // if (command === 'kick') {
-      //   if (!msg.member.hasPermission('KICK_MEMBERS'))
-      //     return msg.reply('You do not have permissions to use that command.');
-      //   if (args.length === 0) return msg.reply('Please provide ID');
-
-      //   const member = msg.guild.members.cache.get(args[0]);
-
-      //   if (member) {
-      //     member
-      //       .kick('misbehaving')
-      //       .then((member) => msg.channel.send(`Bye ${member}.`))
-      //       .catch((err) => {
-      //         console.error(err);
-      //         msg.channel.send('This member is an admin. No can do.');
-      //       });
-      //   } else {
-      //     msg.channel.send('That member was not found.');
-      //   }
       // } else if (command === 'ban') {
       //   if (!msg.member.hasPermission('BAN_MEMBERS'))
       //     return msg.reply('You do not have permissions to use that command.');
