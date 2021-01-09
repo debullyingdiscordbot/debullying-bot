ac9a186f25844657458815c58bd03389122888f5
Michael Chen <michaelchengaming@gmail.com>
2021-01-09 08:56:54 -0800

    fix ban command to update model attribute, logs to console

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require('discord.js');
-const User = require('../database/models/user');
+const { increaseBanCount } = require('../database/MongoDB');
 
 module.exports = {
   name: 'ban',
@@ -17,35 +17,15 @@ module.exports = {
           .then((m) => m.delete({ timeout: 5000 }));
 
       const member = message.guild.members.cache.get(args[0]);
-      // console.log(message.guild.members.cache);
 
       if (member) {
+        await increaseBanCount(message.author.id);
         member.ban().catch((err) => console.error(err));
-        // .then((member) => message.channel.send(`Bye ${member}.`))
-        // .catch((err) => {
-        //   console.error(err);
-        //   message.channel.send('This member is an admin. No can do.');
-        // });
+        // message?
+        client.logger.log(`${message.author.id} has been banned.`);
       } else {
         message.reply('That member was not found.');
       }
-
-      // } else if (command === 'ban') {
-      //   if (!msg.member.hasPermission('BAN_MEMBERS'))
-      //     return msg.reply('You do not have permissions to use that command.');
-      //   if (args.length === 0) return msg.reply('Please provide ID');
-
-      //   msg.guild.members.ban(args[0]).catch((err) => console.log(err));
-      // }
-
-      // console.log(`[${msg.author.tag}]: ${msg.content}`);
-
-      // let embed = new MessageEmbed()
-      //   .setTitle('kick')
-      //   .setDescription('bye')
-      //   .setColor('blue')
-      //   .setFooter('yes');
-      // return message.channel.send(embed);
     } catch (error) {
       console.error(error);
     }
